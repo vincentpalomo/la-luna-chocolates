dc952e5656385143421ab823f3a84781347595c1
Vincent <vincentpalomo@outlook.com>
2024-11-01 17:18:11 -0500

    Update About.tsx
    making adjustment to height of about section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,8 +1,6 @@
-// import Image from 'next/image';
-
 export default function About() {
   return (
-    <section id="about" className="min-h-screen flex flex-col justify-center items-center p-5">
+    <section id="about" className="h-1/2 flex flex-col justify-center items-center p-5">
       <main className="sm:mx-10  mt-20">
         <div className="font-main font-semibold text-xl md:text-6xl leading-tight md:leading-[60px] tracking-[-3%] text-center max-w-[95vw] 2xl:text-[72px] 2xl:leading-[72px] flex items-center justify-center mb-12">
           <div className="flex flex-col sm:items-center items-center">
