099955cc4e4b71f1d9e35667b461aad112531519
Vincent Palomo <vincentpalomo@yahoo.com>
2024-11-01 19:35:30 -0500

    updated issues with events page

--- app/components/Events.tsx
+++ app/components/Events.tsx
@@ -2,9 +2,23 @@ export default function Events() {
   return (
     <section id="events" className="min-h-screen flex flex-col justify-center items-center p-5">
       {/* <h2 className="font-commissioner text-4xl mb-8 sm:text-[200px] sm:mb-20 text-center">EVENTS</h2> */}
+      {/* <div className="sm:w-[95%] block sm:hidden w-full">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&title=La%20Luna%20Chocolate%20Events&mode=AGENDA&src=NjgxODNjMDY4MDk5NmM3N2U5M2QyNGU5YmFhZjk1ODliZDhiMGYxYzlmNWQwN2E0NTE5YmFlNDEyMTBiNzIwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300"
+          width="100%"
+          height="600"
+        ></iframe>
+      </div> */}
+      {/* <div className="sm:w-[95%] hidden sm:block w-full">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&title=La%20Luna%20Chocolate%20Events&mode=MONTH&src=NjgxODNjMDY4MDk5NmM3N2U5M2QyNGU5YmFhZjk1ODliZDhiMGYxYzlmNWQwN2E0NTE5YmFlNDEyMTBiNzIwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300"
+          width="100%"
+          height="600"
+        ></iframe>
+      </div> */}
       <div className="sm:w-[95%] w-full">
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&title=La%20Luna%20Chocolate%20Events&src=NjgxODNjMDY4MDk5NmM3N2U5M2QyNGU5YmFhZjk1ODliZDhiMGYxYzlmNWQwN2E0NTE5YmFlNDEyMTBiNzIwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300"
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&title=La%20Luna%20Chocolate%20Events&mode=MONTH&src=NjgxODNjMDY4MDk5NmM3N2U5M2QyNGU5YmFhZjk1ODliZDhiMGYxYzlmNWQwN2E0NTE5YmFlNDEyMTBiNzIwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F09300"
           width="100%"
           height="600"
         ></iframe>
